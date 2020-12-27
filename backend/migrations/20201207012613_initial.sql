6ed661b3594828b346a2c37480b216e4c5975485
Andrew Halle <ahalle@berkeley.edu>
2020-12-27 14:54:17 -0800

    write a function to add new instances of chore for recurring_chore that are on or past their next_instance_date, and update the next_instance_date of recurring_chore according to repeat_every_days

--- backend/migrations/20201207012613_initial.sql
+++ backend/migrations/20201207012613_initial.sql
@@ -1,25 +1,25 @@
 -- Add migration script here
 create table user (
-  id       INTEGER PRIMARY KEY ASC,
-  username TEXT,
-  password_hash BLOB,
-  password_salt BLOB
+  id       INTEGER PRIMARY KEY ASC NOT NULL,
+  username TEXT NOT NULL,
+  password_hash BLOB NOT NULL,
+  password_salt BLOB NOT NULL
 );
 
 create table recurring_chore (
-  id    INTEGER PRIMARY KEY ASC,
-  title TEXT,
-  repeat_every_days INTEGER,
-  next_instance_date TEXT
+  id    INTEGER PRIMARY KEY ASC NOT NULL,
+  title TEXT NOT NULL,
+  repeat_every_days INTEGER NOT NULL,
+  next_instance_date TEXT NOT NULL
 );
 
 create table chore (
-  id INTEGER PRIMARY KEY ASC,
-  title TEXT,
+  id INTEGER PRIMARY KEY ASC NOT NULL,
+  title TEXT NOT NULL,
   assignee INTEGER,
-  instance_of INTEGER,
-  date TEXT,
-  complete BOOLEAN,
+  instance_of INTEGER NOT NULL,
+  date TEXT NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT false,
   FOREIGN KEY(assignee) REFERENCES user(id)
   FOREIGN KEY(instance_of) REFERENCES recurring_chore(id)
 );
--- backend/src/chore.rs
+++ backend/src/chore.rs
@@ -1,15 +1,75 @@
 use tide::Request;
 use super::State;
 use serde::Deserialize;
+use sqlx::SqlitePool;
+use time::{ Duration, Date};
 
 #[derive(Deserialize, Debug)]
 struct ChoresRequest {
     date: String,
 }
 
+#[derive(Debug, sqlx::FromRow)]
+struct RecurringChore {
+    id: i64,
+    title: String,
+    repeat_every_days: i64,
+    next_instance_date: String,
+}
+
+#[derive(Debug, sqlx::FromRow)]
+struct Chore {
+    id: i64,
+    title: String,
+    assignee: Option<i64>,
+    instance_of: Option<i64>,
+    date: String,
+    complete: bool,
+}
+
+/// For all recurring chores in the database with a next_instance_date on or before
+/// `date`, add a chore instance and move the next_instance_date according to
+/// recurring_chore.repeat_every_days
+async fn add_next_chore_instances_before_date(pool: &SqlitePool, date: &str) -> anyhow::Result<()> {
+    let mut conn = pool.acquire().await?;
+    sqlx::query!("BEGIN").execute(&mut conn).await?;
+    let recurring_chores = sqlx::query_as!(RecurringChore,
+        "SELECT * FROM recurring_chore WHERE next_instance_date <= ?",
+        date
+    )
+        .fetch_all(&mut conn).await?;
+
+    for recurring_chore in recurring_chores {
+        let next_instance_date = Date::parse(&recurring_chore.next_instance_date, "%F")?
+            + Duration::days(recurring_chore.repeat_every_days);
+        let next_instance_date = next_instance_date.format("%Y-%m-%d");
+
+        sqlx::query!(
+            "INSERT INTO
+               chore (title, assignee, instance_of, date)
+             VALUES (?, ?, ?, ?)",
+             recurring_chore.title,
+             None::<i64>,
+             recurring_chore.id,
+             recurring_chore.next_instance_date,
+        ).execute(&mut conn).await?;
+        sqlx::query!(
+            "UPDATE recurring_chore SET next_instance_date = ? WHERE id = ?",
+            next_instance_date,
+            recurring_chore.id,
+        ).execute(&mut conn).await?;
+    }
+
+    sqlx::query!("COMMIT").execute(&mut conn).await?;
+
+    Ok(())
+}
+
 async fn get_chores(req: Request<State>) -> tide::Result {
     let query: ChoresRequest = req.query()?;
-    dbg!(query);
+
+    add_next_chore_instances_before_date(&req.state().db, &query.date).await?;
+
     Ok(r#"{"chores":[]}"#.into())
 }
 
